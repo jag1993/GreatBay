5587546f7d591fb9bfa8c0b830537f0066b3f8f3
Todd Croak-Falen <Todd Croak-Falen>
2016-09-14 21:39:49 -0700

    Pseudo code 'bid' plus some basic actual code.

--- bid.js
+++ bid.js
@@ -13,8 +13,35 @@ connection.connect(function(err) {
 })
 
 
-// If user selects to BID ON AN ITEM:
+// If user selects to BID ON AN ITEM...
+
+// ...show a list of all items:
 connection.query('SELECT * FROM items', function(err, res) {
    if (err) throw err;
    console.log(res);
 })
+
+// Ask user how much they would like to bid:
+
+
+
+// If bid is higher...
+if (userBid > price) {
+   // ...replace the previous price:
+
+   connection.query("UPDATE items SET ? WHERE ?", [{
+      // The new info:
+      price: 
+   }, {
+      // The old info:
+      price: 
+   }], function(err, res) {});
+}
+
+// ELSE the bid is lower...
+else (userBid < price) {
+   // return that the bid was too low and...
+   console.log("Sorry, your bid was too low.");
+   // boot the user back to the selection screen:
+   
+}
